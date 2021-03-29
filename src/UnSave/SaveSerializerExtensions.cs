19353993fdedd8138475ccff128b7cd46a51b592
Alistair Chapman <alistair@agchapman.com>
2021-03-29 14:13:24 +1000

    Add extension methods for serializer;

---
+++ src/UnSave/SaveSerializerExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace UnSave
+{
+    public static class SaveSerializerExtensions
+    {
+        public static GvasSaveData ReadFile(this SaveSerializer serializer, string filePath)
+        {
+            using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return serializer.Read(stream);
+        }
+
+        public static GvasSaveData ReadFile(this SaveSerializer serializer, FileInfo file)
+        {
+            using var stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+            return serializer.Read(stream);
+        }
+
+        public static void WriteToFile(this SaveSerializer serializer, GvasSaveData saveData, string filePath)
+        {
+            using var outStream = File.Open(filePath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
+            serializer.Write(outStream, saveData);
+        }
+
+        public static void WriteToFile(this SaveSerializer serializer, GvasSaveData saveData, FileInfo file)
+        {
+            using var outStream = file.Open(FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
+            serializer.Write(outStream, saveData);
+        }
+
+        public static void WriteToFile(this GvasSaveData saveData, SaveSerializer serializer, string filePath)
+        {
+            using var outStream = File.Open(filePath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
+            serializer.Write(outStream, saveData);
+        }
+    }
+}
